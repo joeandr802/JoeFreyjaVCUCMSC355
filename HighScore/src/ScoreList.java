58c3c5b112fe25386a10dbc16446e176c8a46026
LAPTOP-U0O6V5L6\ransa <ransam0310@gmail.com>
2021-11-23 21:53:22 -0500

    deleting random readme's I added while starting work on the highscore functionality

---
+++ HighScore/src/ScoreList.java
@@ -0,0 +1,13 @@
+
+public class ScoreList {
+    HighScore[] list = new HighScore[10];
+    final int SIZE = 10;
+
+    /**ScoreList(put 10 Highscore classes here){
+
+    }
+
+
+
+     */
+}
---
+++ HighScore/src/empty2.java
@@ -0,0 +1,2 @@
+public class empty2 {
+}
