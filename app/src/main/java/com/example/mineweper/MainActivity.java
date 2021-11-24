d202b99639930332baecbe6dee0f6524621cb553
Sull <taylorac3@vcu.edu>
2021-11-24 01:41:13 -0500

    Added a very important splash screen sound.

--- app/src/main/java/com/example/mineweper/MainActivity.java
+++ app/src/main/java/com/example/mineweper/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.content.Intent;
+import android.media.MediaPlayer;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                //Sound effect
+                player = MediaPlayer.create(MainActivity.this, R.raw.scramble);
+                player.start();
+
                 Intent intent = new Intent(MainActivity.this, Menu0.class);
                 startActivity(intent);
                 finish();
